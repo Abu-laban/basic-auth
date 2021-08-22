462488e9830f38e62aea59283258d2f1be2ac033
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-08-22 21:33:49 +0300

    finish_lab06

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('dotenv').config();
+const { start } = require('./src/server');
+const { db } = require('./src/models/index');
+
+const PORT = process.env.PORT;
+
+db.sync()
+    .then(() => {
+        start(PORT);
+    }).catch(e => {
+        console.error('Could not start server', e.message);
+    });
